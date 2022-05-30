19a37ba82001eef299e6c284ef5418870f096c70
Dima Shlapak <dimashlapak2255@gmail.com>
2022-05-30 17:03:31 +0300

    added FinalHWShlapakDima

---
+++ homeworks/Shlapak/Final-homeworks/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('.burger-menu').click(function() {
+        $('#nav').toggleClass('open'); 
+});
+});
+$(document).ready(function() {
+    $('.burger-menu').click(function() {
+        $('.wrapper').toggleClass('wrapper-active'); 
+});
+});
+$(document).ready(function() {
+    $('.burger-menu').click(function() {
+        $('.burger-menu').toggleClass('active-button'); 
+});
+});
+$(document).ready(function() {
+        $('.slider').slick({
+            arrows:false,
+            dots:true,
+        }); 
+});
