34a51f010dd097d8df0eaceed4db48a9631ab71c
User <user@MacBook-Air-User.local>
2022-06-12 17:42:34 +0300

    final-work

--- homeworks/borovschih/final-work/script.js
+++ homeworks/borovschih/final-work/script.js
@@ -1,5 +1,23 @@
-$(document).ready(function() {
-    $('.burger').click(function(event) {
-        $('.burger,.header-menu').toggleClass('active');
-    });
-});
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  console.log();
+})
+
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
