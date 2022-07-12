ed0517e9b6888399733a2b53a41502e3a533aead
Nataliia Tereshchenko <Nataliia.Snovida@gmail.com>
2022-07-12 17:46:04 +0300

    homework 1 done

---
+++ homework-1/task4.js
@@ -0,0 +1,7 @@
+//Конкатенація
+let clientPhrase1 = prompt('Напишіть щось');
+let clientPhrase2 = prompt('Напишіть ще щось');
+let clientPhrase3 = prompt('І ще щось :)');
+
+let resultPhrase = clientPhrase1 + " " + clientPhrase2 + " " + clientPhrase3;
+alert(resultPhrase);
