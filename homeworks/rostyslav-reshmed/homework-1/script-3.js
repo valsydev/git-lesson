1d8ce0859c811fe813e5f5a465c7baa089e73c51
Reshmed <reshmed@i.ua>
2022-07-13 11:14:39 +0300

    homework 1 done

---
+++ homeworks/rostyslav-reshmed/homework-1/script-3.js
@@ -0,0 +1,5 @@
+let c = prompt("Введіть ПЕРШЕ число");
+    d = prompt("Введіть ДРУГЕ число");
+    f = prompt("Введіть ТРЕТЄ число");
+const medium = 3;
+alert("Середнє арефметичне трьох чисел" + " " + ((Number(c) + Number(d) + Number(f)) / medium))
---
+++ homeworks/rostyslav-reshmed/homework-1/script-4.js
@@ -0,0 +1,4 @@
+let firstName = prompt ("Введіть Ваше ім'я");
+    lastName = prompt ("Введіть Ваше прізвище");
+    userAge = prompt ("Скільки Вам років?");
+alert(firstName + " " + lastName + " " + "Вам" + " " + userAge + " " + "років.")
