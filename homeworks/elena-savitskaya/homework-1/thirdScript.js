0965e15e68884f7b9ced43e16ff11c13ccba8ccf
ElenaS35 <helena0518savitskaya@gmail.com>
2022-07-13 15:10:18 +0200

    homework-1 done

---
+++ homeworks/elena-savitskaya/homework-1/thirdScript.js
@@ -0,0 +1,7 @@
+let k = +prompt("Enter the first number", "");
+let m = +prompt("Enter the second number", "");
+let n = +prompt("Enter the third number", "");
+
+let average = ((k + m + n) / 3);
+
+alert(average);
