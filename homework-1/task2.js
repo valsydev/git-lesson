ecac7643a12f684ba36b54b8129e6a00e53302c8
Nataliia Tereshchenko <Nataliia.Snovida@gmail.com>
2022-07-20 14:05:38 +0300

    Remove folder

--- homework-1/task2.js
+++
@@ -1,7 +0,0 @@
-//Порівняння значень
-let clientValue1 = prompt('Введіть будь-яке значення для порівняння');
-let clientValue2 = prompt('Введіть ще якесь значення для порівняння');
-
-let isTheSameValue = clientValue1 === clientValue2 ? true : false;
-
-alert(isTheSameValue);
--- homework-1/task3.js
+++
@@ -1,8 +0,0 @@
-//Середнє арифметичне
-let clientNumber1 = +prompt('Введіть перше число для визначення середнього арифметичного');
-let clientNumber2 = +prompt('Введіть друге число для визначення середнього арифметичного');
-let clientNumber3 = +prompt('Введіть третє число для визначення середнього арифметичного');
-
-let average = (clientNumber1 + clientNumber2 + clientNumber3)/3;
-
-alert(average);
