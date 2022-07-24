eab8e0637b6e3a42e9c2f33b48dbe8000e4a1af1
Olha Nevzghodova <onevz.alex@gmail.com>
2022-07-25 01:23:27 +0300

    add homework

---
+++ homeworks/olha-nevzghodova/homework-1/script-4.js
@@ -0,0 +1,4 @@
+const country = prompt('Please enter your country', '');
+const region = prompt('Please enter your region', '');
+const locality = prompt('Please enter your locality', '');
+alert(country + ',' + region + ',' + locality);
