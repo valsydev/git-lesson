ca44abf61e6574bc8eaf9a2ca28bf1eca4b28af9
Shmalenko <sanyashmalenko@icloud.com>
2022-07-26 15:20:52 +0300

    hm1-Shmalenko

---
+++ homeworks/alexandr-shmalenko/homework1/fourth.js
@@ -0,0 +1,5 @@
+const firstName = prompt('Enter firstName (имя): ');
+const secondName = prompt('Enter secondName (фамилия): ');
+const patronymic = prompt('Enter patronymic (отчество): ');
+document.write(secondName + ' ' + firstName + ' ' + patronymic);
+alert(secondName + ' ' + firstName + ' ' + patronymic);
